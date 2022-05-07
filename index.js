fcc0fa24fc68214d25da5cce2aadf7acf882391c
sadik126 <sadikhimel04@gmail.com>
2022-05-07 17:12:36 +0600

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('running');
+})
