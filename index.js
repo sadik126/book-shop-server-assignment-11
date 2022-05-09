41496b1ad000dac28b86a3edb34c42d29eb8cba0
sadik126 <sadikhimel04@gmail.com>
2022-05-09 13:59:16 +0600

    m

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //MIDDLEWARE
 app.use(cors())
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('running');
+});
+
+app.listen(port, () => {
+    console.log('crud is running')
+})
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.jkugq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
 });
