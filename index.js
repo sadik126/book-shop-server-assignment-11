bd497dce108af1a190fa0bbf2ca2354cb8115846
sadik126 <sadikhimel04@gmail.com>
2022-05-09 13:59:12 +0600

    m

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 
 //MIDDLEWARE
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('running');
-})
+});
